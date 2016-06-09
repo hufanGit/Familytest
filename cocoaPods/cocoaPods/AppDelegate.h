6543c3de963f3aa8660dc44bf397cb318d4ad23f
胡凡 <hufan@hufandeMacBook-Pro.local>
2016-06-09 18:37:53 +0800

    第一次测试

---
+++ cocoaPods/cocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoaPods
+//
+//  Created by 胡凡 on 16/6/9.
+//  Copyright © 2016年 胡凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoaPods/cocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoaPods
+//
+//  Created by 胡凡 on 16/6/9.
+//  Copyright © 2016年 胡凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
